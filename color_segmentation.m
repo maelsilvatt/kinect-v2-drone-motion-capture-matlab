a59fa6da6bd2e6569135862c5f373cd72653e388
Ismael Soares <ismaelsoares.pro@gmail.com>
2024-04-25 09:26:18 -0300

    Correction on red mask filter values

--- color_segmentation.m
+++ color_segmentation.m
@@ -1,52 +1,46 @@
-close all; % DEBUG: fecha todas as janelas ativas
+close all;
 
-%% Carregar a imagem
-image = imread('input_image.png');
+%% Load drone image
+image = imread('images/marked-parrot-drone.png');
 
-%% Conversão RGB para HSV
+%% Convert from RGB to HSV
 hsv_image = rgb2hsv(image);
 
-%% Definir os valores HSV da cor desejada (vermelho)
-h_color = 0.1; % matiz média
-s_min = 0.65; % saturação mínima
-v_min = 0.7; % valor mínimo
-v_max = 0.9; % valor máximo
+%% Set the desired HSV tone (in this case, red color)
+h_color = 0.1; % average hue
+s_min = 0.5; % min saturation
+v_min = 0.5; % min value
+v_max = 1.0; % max value
 
-%% Criar a máscara baseada nos valores HSV definidos
-color_threshold = 1;
+%% Set the color mas
+color_threshold = .5; % to get close variations
 mask = (hsv_image(:,:,1) >= (h_color - color_threshold)) & ...
           (hsv_image(:,:,1) <= (h_color + color_threshold)) & ...
           (hsv_image(:,:,2) >= s_min) & ...
           (hsv_image(:,:,3) >= v_min) & ...
           (hsv_image(:,:,3) <= v_max);
 
-%% Exibir a máscara
-%imshow(mask);
-
-%% Aplicar a máscara à imagem original
+%% Apply mask to drone image
 color_mask = bsxfun(@times, image, cast(mask, 'like', image));
-%imshow(color_mask);
 
-% %% Aplicar erosão à máscara
-% se1 = strel('disk', 10); % raio do disco de erosão
-% im_erod = imerode(color_mask, se1);
-% %imshow(im_erod);
-% 
-% %% Aplicar dilação à máscara
-% se2 = strel('disk', 30); % raio do disco de dilação
-% im_dil = imdilate(im_erod, se2);
-% %imshow(im_dil);
+%% Apply erosion
+se1 = strel('disk', 10); % erosion format and radius
+im_erod = imerode(color_mask, se1);
 
-%% Binarização da imagem
-bw_threshold = 0.1; % limiar de binarização
+%% Apply dilation
+se2 = strel('disk', 10); % dilation format and radius
+im_dil = imdilate(im_erod, se2);
+
+%% Binarize mask
+bw_threshold = 0.1; % binarization threshold
 gray_image = rgb2gray(im_dil);
 bw_image = imbinarize(gray_image, bw_threshold);
-imshow(bw_image);
 
-%% Obtendo o centroide do círculo pela Transformada de Hough
-[centroid, radii] = imfindcircles(bw_image, [30, 150]);
+%% Get disk's centroid through Hough Transformation
+c_range = [200, 1000];
+[centroid, radii] = imfindcircles(bw_image, c_range);
 
-% Exiba a imagem com os círculos detectados
-%imshow(image);
-%viscircles(centroid, radii);
+%% Show drone's centroid
+imshow(image);
+viscircles(centroid, radii);
 
