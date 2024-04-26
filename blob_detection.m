988ce4c5f5efbebf5c47d737ab2a24dbc8dfddf4
Ismael Soares <ismaelsoares.pro@gmail.com>
2024-04-26 12:10:22 -0300

    Update on code structure

--- blob_detection.m
+++ blob_detection.m
@@ -1,41 +1,35 @@
-close all;
-
-%% Load drone image
+%% Step 1: Load and preprocess the image
 image = imread('images/marked-parrot-drone.png');
-
-%% Convert from RGB to HSV
 hsv_image = rgb2hsv(image);
 
-%% Set the desired HSV tone (in this case, red color)
+%% Step 2: Create a color mask by filtering HSV channels
 h_color = 0.1; % average hue
 s_min = 0.5; % min saturation
 v_min = 0.5; % min value
 v_max = 1.0; % max value
 
-%% Set the color mas
-color_threshold = .5; % to get close variations
+color_threshold = .5;
 mask = (hsv_image(:,:,1) >= (h_color - color_threshold)) & ...
           (hsv_image(:,:,1) <= (h_color + color_threshold)) & ...
           (hsv_image(:,:,2) >= s_min) & ...
           (hsv_image(:,:,3) >= v_min) & ...
           (hsv_image(:,:,3) <= v_max);
 
-%% Apply mask to drone image
+%% Step 3: Apply the color mask on the image
 color_mask = bsxfun(@times, image, cast(mask, 'like', image));
 
-%% Binarize mask
+%% Step 4: Binarize the filtered image
 bw_threshold = 0.1;  % binarization threshold
 gray_image = rgb2gray(color_mask);
 bw_image = imbinarize(gray_image, bw_threshold);
 
-%% Remove noises
+%% Step 5: Remove noises
 morph_disk = strel('disk', 10);             % set morphological structure
 im_open = imopen(bw_image, morph_disk);  % apply oppening
 
-%% Get disk's centroid through Hough Transformation
+%% Step 6: Get blob's centroid through Hough Transformation
 c_range = [40, 400];
 [centroid, radii] = imfindcircles(im_open, c_range);
 
-%% Show drone's centroid
 imshow(image);
 viscircles(centroid, radii);
