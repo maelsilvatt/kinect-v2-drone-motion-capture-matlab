03142bc04fae495b4f030c773008758909767ac9
Ismael Soares <ismaelsoares.pro@gmail.com>
2024-05-07 15:24:22 -0300

    Major update on color detection code

--- blob_detection.m
+++ blob_detection.m
@@ -1,10 +1,11 @@
 function [centroid, radii] = blob_detection(image, c_range)
-    [bw_image, ~] = create_mask(image);
+    [bw_image, mask] = create_mask(image);
 
     % Remove noises
-    morph_disk = strel('disk', 5);          % set morphological structure
+    morph_disk = strel('disk', 5);           % set morphological structure
     im_open = imopen(bw_image, morph_disk);  % apply opening
 
     % Step 6: Get blob's centroid through Hough Transformation
     [centroid, radii] = imfindcircles(im_open, c_range);
+    % [centroid, radii] = imfindcircles(bw_image, c_range);
 end
--- main.m
+++ main.m
@@ -1,5 +1,5 @@
-%% Clear flags
-imaqreset; warning('OFF'); clear all; close all; clc;
+%% Settings
+warning('off');
 
 %% Setting up Kinect V2
 % fprintf('Configuring Kinect V2...');
@@ -19,18 +19,21 @@ imaqreset; warning('OFF'); clear all; close all; clc;
 vid = VideoReader('images\parrot-drone-video.mp4');
 
 % Create an object to save output video
-videoOut = VideoWriter('output_video.mp4', 'MPEG-4');
+delete('output\output_video.mp4');
+videoOut = VideoWriter('output\output_video.mp4', 'MPEG-4');
 open(videoOut);
 
 % Set a frame counter
 frame_count = 0;
 
 % Sets Hough Transform range
-c_range = [30, 300];
+c_range = [1, 700];
 
 while hasFrame(vid)
     % Increment frame counter
     frame_count = frame_count + 1;
+
+    fprintf('Processing frame %d of %d (%d%%)\n', frame_count, vid.NumFrames, floor((frame_count/vid.NumFrames)*100));
     
     % Get video frame
     frame = readFrame(vid);
@@ -40,19 +43,21 @@ while hasFrame(vid)
 
     % Detects if the object is ocluded or not
     if size(centroid) > 0
-        centroid_text = sprintf('Centroid: (%d, %d)', centroid(1), centroid(2));
+        centroid_info = sprintf('Centroid: (%.2f, %.2f)', centroid(1), centroid(2));
     else
-        text = sprintf('Centroid: N/A');
+        centroid_info = sprintf('Centroid: N/A');
     end
 
-    % Inserts current frame
-    frame = insertText(frame, [1, 1], frame_count, FontSize=24);
+    % Inserts a circle where the blob was detected
+    [max_radius, max_index] = max(radii); % highest radii index
+    biggest_centroid = centroid(max_index, :); % highest radii coords
+    frame = insertShape(frame, 'filled-circle', [biggest_centroid, max_radius], 'Color', 'red');
 
     % Inserts a text showing detected blob centroid
-    frame = insertText(frame, [1, -36], centroid_text, FontSize=36, FontColor="white", TextBoxColor="black");
+    frame = insertText(frame, [1, 50], centroid_info, FontSize=36, FontColor="white", TextBoxColor="black");
 
-    % Inserts a circle where the blob was detected
-    frame = insertShape(frame, 'filled-circle', [centroid, radii], 'Color', 'red');
+    % Inserts current frame counter on frame
+    frame = insertText(frame, [1, 1], frame_count, FontSize=24);
 
     % Write frame into output video
     writeVideo(videoOut, frame);
@@ -63,3 +68,5 @@ close(videoOut);
 
 % Cleans input video handler
 delete(vid);
+
+fprintf('Done!\n');
